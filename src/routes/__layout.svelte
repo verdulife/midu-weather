0ddf1e45c0a34fe9835b00e55145c55aa839b38c
gaplogic <estudio@gaplogic.com>
2022-05-23 12:40:02 +0200

    project base start

---
+++ src/routes/__layout.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import 'verdu/fonts/circular.css';
+	import 'verdu/fonts/operator.css';
+</script>
+
+<div id="app" class="row fcenter fill">
+	<slot />
+</div>
+
+<style lang="scss">
+	:global {
+		@import '../_reset.scss';
+		@import 'verdu/verdu.scss';
+	}
+</style>
